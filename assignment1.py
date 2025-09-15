8b135c6d20d6d916fe0fda3cfc30e3d4e2f58a29
alyssamil26 <alyssamil26@gmail.com>
2025-09-14 22:15:05 -0400

    Update assignment1.py

---
+++ assignment1.py
@@ -0,0 +1,22 @@
+# Import libraries/functions
+from pygam import LinearGAM, s, f, l
+import pandas as pd
+
+# Read in data
+data = pd.read_csv("https://github.com/dustywhite7/econ8310-assignment1/raw/main/assignment_data_train.csv")
+data["Timestamp"] = pd.to_datetime(data["Timestamp"])
+
+#variables
+x = data[['year', 'month', 'day', "hour"]]
+y = data['trips']
+
+model = LinearGAM(
+    s(0) +
+    f(1) +
+    f(2) +
+    s(3)
+)
+
+modelfit = model.gridsearch(x.values, y)
+
+pred = model.predict(x)
